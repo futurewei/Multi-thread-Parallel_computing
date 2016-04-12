c7730f25932c9aca7e0e79a1ba6497ee232698b1
laiwei <laiwei@berkeley.edu>
2016-04-12 01:56:36 -0700

    Try increment 8 each time

--- calcDepthOptimized.c
+++ calcDepthOptimized.c
@@ -28,12 +28,12 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
 	//depth array size= imageheight * imagewidth
 	int b;
 	__m128 zero=_mm_setzero_ps();
-	for (int w = 0; w < imageWidth*imageHeight/4*4; w+=4)
+	for (int w = 0; w < imageWidth*imageHeight/8*8; w+=8)
 	{	
 			_mm_storeu_ps(&depth[w], zero);
 	}
 		//tail case:
-	for (b = imageWidth*imageHeight/4*4;  b<= imageWidth*imageHeight; b++)
+	for (b = imageWidth*imageHeight/8*8;  b<= imageWidth*imageHeight; b++)
 	{	
 			depth[b] = 0;
 	}
