205e1dd94c25527d07cfb03c6efaa6e6ee17b2d5
laiwei <laiwei@berkeley.edu>
2016-04-11 23:26:45 -0700

    core dumped bug?

--- calcDepthOptimized.c
+++ calcDepthOptimized.c
@@ -26,7 +26,6 @@ void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth,
 {
 	/* The two outer for loops iterate through each pixel */
 	//depth array size= imageheight * imagewidth
-	#pragma omp parallel for
 	for (int h = 0; h < imageHeight; h++)
 	{
 		for (int w = 0; w < imageWidth; w++)
